8cf5798145259d3e2a7a2ac5d291fff7d0379271
Sreerag Sridharan <sreeragsridharan@Sreerags-MacBook-Pro.local>
2025-11-09 18:34:29 +0530

    Add Tailwind styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // scan all JS/TS/React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
